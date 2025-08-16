5708d5019cbf3189fbe9f107014c904de3c3dcf7
Sadikur Rahman <sadikur.rahman@bevycommerce.com>
2025-08-16 17:34:01 -0400

    re-added project nav

--- src/components/portfolio/FloatingNav.tsx
+++ src/components/portfolio/FloatingNav.tsx
@@ -11,7 +11,7 @@ const FloatingNav = () => {
     { id: 'hero', label: 'Home' },
     { id: 'about', label: 'About' },
     { id: 'skills', label: 'Skills' },
-    // { id: 'projects', label: 'Projects' },
+    { id: 'projects', label: 'Projects' },
     { id: 'experience', label: 'Experience' },
     { id: 'contact', label: 'Contact' }
   ];
